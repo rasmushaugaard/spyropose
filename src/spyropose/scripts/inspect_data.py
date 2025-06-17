90b764c0169200cf524c023372867ebceb6f671f
Rasmus Haugaard <raha@mmmi.sdu.dk>
2025-06-17 15:53:58 +0200

    Show keypoints in inspect_data

--- src/spyropose/scripts/inspect_data.py
+++ src/spyropose/scripts/inspect_data.py
@@ -12,14 +12,17 @@ from ..data.dataset import SpyroDataset
 
 def main(
     data: SpyroDataConfig,
-    pts_alpha=0.7,
     draw_frame=True,
+    draw_kpts=True,
+    draw_verts=False,
+    marker_size=10,
+    marker_thickness=2,
     same_inst=False,
     seed=0,
 ):
     dataset = SpyroDataset(data)
-    pts = trimesh.load_mesh(data.obj.mesh_path).vertices - data.obj.frame.obj_t_frame
-
+    verts = trimesh.load_mesh(data.obj.mesh_path).vertices - data.obj.frame.obj_t_frame
+    kpts = data.obj.keypoints
     np.random.seed(seed)
 
     axs = plt.subplots(4, 4, figsize=(10, 10))[1]
@@ -34,22 +37,29 @@ def main(
         R = d["R"]
         t = d["t"]
 
-        vts = R @ pts.T + t
-        p = K @ vts
-        p = p[:2] / p[2:]
-        u, v = np.round(p).astype(int).clip(0, data.obj.crop_res - 1)
-        im[v, u] = (1 - pts_alpha) * im[v, u] + pts_alpha * np.array((1, 0, 0))
+        for p, do_draw in (verts, draw_verts), (kpts, draw_kpts):
+            if not do_draw:
+                continue
+            p = K @ (R @ p.T + t)
+            p = p[:2] / p[2:]
+            cmap = plt.get_cmap("tab20")
+            for ci, p in enumerate(np.round(p).astype(int).T):
+                c = cmap(ci % cmap.N)
+                cv2.drawMarker(im, p, c, cv2.MARKER_TILTED_CROSS, marker_size, marker_thickness)
 
         if draw_frame:
             # cv2 assumes K to be upper triangular with positive diagonals
+            # due to image augmentations, this is not necessarily true.
+            # One could have changed the image augmentations to rotate the camera instead of the intrinsics. Anyways:
             # u = K (Rp + t) = K_(R_Rp + R_t)
             K_, R_ = scipy.linalg.rq(K)
+            K_: np.ndarray
             sign = np.sign(K_.diagonal())
             K_ = K_ * sign.reshape(1, 3)
             R_ = R_ * sign.reshape(3, 1)
             assert np.allclose(K_ @ R_, K), np.linalg.norm(K_ @ R_ - K)
             im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
-            cv2.drawFrameAxes(  # type: ignore
+            cv2.drawFrameAxes(
                 image=im,
                 cameraMatrix=K_,
                 rvec=cv2.Rodrigues(R_ @ R)[0],
